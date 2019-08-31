8e3991adc1883fc3ebdbac9755eb198c776ab9bf
David Puddefoot <david@fluid2.com>
2019-08-31 23:59:54 +0100

    adds template files for project - still plenty to do

---
+++ resources/views/core/sidebar.blade.php
@@ -0,0 +1,21 @@
+<ul class="nav">
+    <li class="nav-item">
+        <a class="nav-link" href="/">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/clients">Clients</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+</ul>
+
